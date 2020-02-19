36daa1fe5a7cc3993d7e850fbd8b40c074a08565
JackPu <kakashjack@gmail.com>
2020-02-19 16:02:26 +0800

    docs: add en doc

---
+++ public/source.js
@@ -0,0 +1,15 @@
+const videoSource = [
+  {
+    src: 'https://media.vued.vanthink.cn/sparkle_your_name_am360p.mp4',
+    resolution: 360,
+  }, {
+    src: 'https://media.vued.vanthink.cn/sparkle_your_name_am720p.mp4',
+    resolution: 720,
+  }, {
+    src: 'https://media.vued.vanthink.cn/y2mate.com%20-%20sparkle_your_name_amv_K_7To_y9IAM_1080p.mp4',
+    resolution: 1080
+  }
+]
+
+
+const videoSource = 'https://media.vued.vanthink.cn/the_garden_of_words_trailer_english__1080p.mp4'
