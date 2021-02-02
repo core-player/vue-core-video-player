56b7092944a77ed8e39a267b5fe693d0bf0154fa
Janne Hamalainen <janne.a.hamalainen@gmail.com>
2021-02-02 20:11:47 +0200

    Improvement for tips/hint support
    Added tip for settings, mute and layer pause/play buttons. Added new localization string for settings for all
    languages. Japanese and Chinese strings translated with google. Tips banner is shown on a bit lower position on
    dashboard so that it wont be hidden e.g. by the volume control.

--- src/dashboard/volume.vue
+++ src/dashboard/volume.vue
@@ -2,9 +2,11 @@
   <div class="vue-core-video-player-control volume-control">
     <div class="btn-control btn-volume" v-if="!isMuted" @click="toggle">
       <svg xmlns="http://www.w3.org/2000/svg" width="23.542" height="23" viewBox="0 0 23.542 23"><path data-name="15" fill="#fff" d="M0 5.5h7v12H0z"/><path data-name="3" d="M.5 11.5L12.5 0v23z" fill="#fff"/><g data-name="12 1" fill="none" stroke="#fff" stroke-width="1.5"><path data-name="2" d="M15.787 8.349a2.89 2.89 0 0 1 3.04 3.126 2.763 2.763 0 0 1-3.142 2.833" stroke-width="1.50021"/><path data-name="3" d="M16.052 4.807s6.917-.147 6.61 6.796-6.83 6.16-6.83 6.16" stroke-width="1.50021"/></g></svg>
+      <div class="tips">{{$t('dashboard.btn.mute')}}</div>
     </div>
     <div class="btn-control btn-mute" v-if="isMuted" @click="toggle">
       <svg xmlns="http://www.w3.org/2000/svg" width="24.485" height="23" viewBox="0 0 24.485 23"><g fill="#fff"><path data-name="矩形 15" d="M0 5.5h7v12H0z"/><path data-name="多边形 3" d="M.5 11.5L12.5 0v23z"/><g data-name="组 3"><path data-name="矩形 39" d="M23.071 7.257l1.414 1.414-7.07 7.071L16 14.328z"/><path data-name="矩形 40" d="M16 8.672l1.414-1.414 7.071 7.07-1.414 1.415z"/></g></g></svg>
+      <div class="tips">{{$t('dashboard.btn.unmute')}}</div>
     </div>
 
     <div class="btn-control-panel">
